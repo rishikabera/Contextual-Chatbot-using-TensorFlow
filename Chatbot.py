9497ad9eb3923066a5a832d6d16eff1b3c262223
rishikabera <55071483+rishikabera@users.noreply.github.com>
2020-09-10 15:47:49 -0700

    Updated  Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,1 +1,26 @@
+# Libraries needed for NLP
+import nltk
+nltk.download('punkt')
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
 
+# Libraries needed for Tensorflow processing
+import tensorflow as tf
+import numpy as np
+import tflearn
+import random
+import json
+
+from google.colab import files
+files.upload()
+
+# import our chat-bot intents file
+with open('intents.json') as json_data:
+    intents = json.load(json_data)
+
+intents
+
+words = []
+classes = []
+documents = []
+ignore = ['?']

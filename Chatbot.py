6f809ed15b6c30eea456f7731deae0260dc9fbb1
rishikabera <55071483+rishikabera@users.noreply.github.com>
2020-09-11 17:35:53 -0700

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -84,4 +84,148 @@ train_y = list(training[:,1])
 # resetting underlying graph data
 tf.reset_default_graph()
 
+# Building neural network
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 10)
+net = tflearn.fully_connected(net, 10)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net)
 
+# Defining model and setting up tensorboard
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+
+# Start training
+model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
+model.save('model.tflearn')
+
+import pickle
+pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
+
+# restoring all the data structures
+data = pickle.load( open( "training_data", "rb" ) )
+words = data['words']
+classes = data['classes']
+train_x = data['train_x']
+train_y = data['train_y']
+
+with open('intents.json') as json_data:
+    intents = json.load(json_data)
+
+# load the saved model
+model.load('./model.tflearn')
+
+def clean_up_sentence(sentence):
+    # tokenizing the pattern
+    sentence_words = nltk.word_tokenize(sentence)
+    # stemming each word
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# returning bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words, show_details=False):
+    # tokenizing the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # generating bag of words
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+
+    return(np.array(bag))
+
+ERROR_THRESHOLD = 0.30
+def classify(sentence):
+    # generate probabilities from the model
+    results = model.predict([bow(sentence, words)])[0]
+    # filter out predictions below a threshold
+    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((classes[r[0]], r[1]))
+    # return tuple of intent and probability
+    return return_list
+
+def response(sentence, userID='123', show_details=False):
+    results = classify(sentence)
+    # if we have a classification then find the matching intent tag
+    if results:
+        # loop as long as there are matches to process
+        while results:
+            for i in intents['intents']:
+                # find a tag matching the first result
+                if i['tag'] == results[0][0]:
+                    # a random response from the intent
+                    return print(random.choice(i['responses']))
+
+            results.pop(0)
+
+classify('What are you hours of operation?')
+
+response('What are you hours of operation?')
+
+response('What is menu for today?')
+
+#Some of other context free responses.
+response('Do you accept Credit Card?')
+
+response('Where can we locate you?')
+
+response('That is helpful')
+
+response('Bye')
+
+#Adding some context to the conversation i.e. Contexualization for altering question and intents etc.
+# create a data structure to hold user context
+context = {}
+
+ERROR_THRESHOLD = 0.25
+def classify(sentence):
+    # generate probabilities from the model
+    results = model.predict([bow(sentence, words)])[0]
+    # filter out predictions below a threshold
+    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((classes[r[0]], r[1]))
+    # return tuple of intent and probability
+    return return_list
+
+def response(sentence, userID='123', show_details=False):
+    results = classify(sentence)
+    # if we have a classification then find the matching intent tag
+    if results:
+        # loop as long as there are matches to process
+        while results:
+            for i in intents['intents']:
+                # find a tag matching the first result
+                if i['tag'] == results[0][0]:
+                    # set context for this intent if necessary
+                    if 'context_set' in i:
+                        if show_details: print ('context:', i['context_set'])
+                        context[userID] = i['context_set']
+
+                    # check if this intent is contextual and applies to this user's conversation
+                    if not 'context_filter' in i or \
+                        (userID in context and 'context_filter' in i and i['context_filter'] == context[userID]):
+                        if show_details: print ('tag:', i['tag'])
+                        # a random response from the intent
+                        return print(random.choice(i['responses']))
+
+            results.pop(0)
+
+response('Can you please let me know the delivery options?')
+
+response('What is menu for today?')
+
+context
+
+response("Hi there!", show_details=True)
+
+response('What is menu for today?')
